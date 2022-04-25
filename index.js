b71e978070e3b916f4877a28b33d6f50d015aa65
Gus Bunce <bunce627@gmail.com>
2022-04-25 15:53:36 +1000

    Starting to add requests

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-// Enviromental variables
+// Import modules
+const express = require('express');
 require('dotenv').config();
 
+//Create expess app
+const app = express();
+
+// Start the API
+app.listen(3000, () => {
+    console.log('Api is accessible on port 3000');
+})
+
+// Call the curseforge API
+const headers = {
+    'Accept': 'application/json',
+    'x-api-key': process.env.API_KEY
+}
+
+//Piggyback API listners
+app.get('/getgameinfo', (req, res, next) => {
+    
+})
